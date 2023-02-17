5c32d2d45adca9101cdfe74e8046dc7fd1331d35
Adam Ibrahim <muhadamibrahim1873@gmail.com>
2023-02-17 23:17:44 +0700

    adam

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+
+search.addEventListener('click', () =>{
+    
+    const APIKey = '551a3706a971de56c3e05ca809ba9085';
+    const city = document.querySelector('.search-box input').velue;
+
+    if (city === '')
+        return;
+    
+})
